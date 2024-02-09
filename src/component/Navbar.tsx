b83adcd9e6630d28a6afff63f6851eb49a94341f
Gyuri Kim <rnfl0318@naver.com>
2024-02-09 16:38:34 +0900

    nav 작업 :모바일, 웹

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react"
 import Link from "next/link"
 
+import { IoIosMenu } from "react-icons/io";
+import { IoCloseOutline } from "react-icons/io5";
+
+
 export default function Navbar(){
     const [isOpen,setIsOpen]=useState(false)
     return(
@@ -15,9 +19,21 @@ export default function Navbar(){
                 <Link href="/users/likes" className="navbar__list--item">찜한 가게</Link>
                 <Link href="/users/login" className="navbar__list--item">로그인</Link>
             </div>
+            {/* 모바일전용 버튼 */}
             <div role="presentation" className="navbar__button" onClick={()=>setIsOpen((val)=>!val)}>
-                {isOpen}
+                {isOpen?<IoCloseOutline/>: <IoIosMenu/>}
             </div>
+            {/* 모바일 navbar */}
+            {isOpen&&(
+                <div className="navbar--mobile">
+                    <div className="navbar__list--mobile">
+                        <Link href="/stores" className="navbar__list--item--mobile">맛집 목록</Link>
+                        <Link href="/stores/new" className="navbar__list--item--mobile">맛집 등록</Link>
+                        <Link href="/users/likes" className="navbar__list--item--mobile">찜한 가게</Link>
+                        <Link href="/users/login" className="navbar__list--item--mobile">로그인</Link>
+                    </div>
+                </div>
+            )}
          </div>
        </>
     )
