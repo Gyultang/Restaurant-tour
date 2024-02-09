afb28bfce76a5d0a2347533bb367fda8b3ea2e1a
Gyuri Kim <rnfl0318@naver.com>
2024-02-09 18:46:24 +0900

    kakao map 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,10 @@
-import Link from "next/link";
-
-
+import Map from "@/components/Map";
 export default function Home() {
+
+
   return (
     <>
-      <h1 className="font-bold">Map Index Page</h1>
-      <ul>
-        <li>
-          <Link href="/stores">맛집목록</Link>
-        </li>
-        <li>
-          <Link href="/stores/new">맛집생성</Link>
-        </li>
-        <li>
-          <Link href="/stores/1">맛집상세페이지</Link>
-        </li>
-        <li>
-          <Link href="/stores/1/edit">맛집수정페이지</Link>
-        </li>
-        <li>
-          <Link href="/users/login">로그인 페이지</Link>
-        </li>
-        <li>
-          <Link href="/users/mypage">마이페이지</Link>
-        </li>
-        <li>
-          <Link href="/users/likes">찜한 맛집</Link>
-        </li>
-      </ul>
+      <Map/>
     </>
   );
 }
