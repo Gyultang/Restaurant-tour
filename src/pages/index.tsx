01bdc3cfbc2f20357866e8d7a0e455249ca31b96
Gyuri Kim <rnfl0318@naver.com>
2024-02-15 23:08:07 +0900

    matker 비지니스로직 분리

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import Map from "@/components/Map";
+import Markers from"@/components/Markers";
+import * as stores from "@/data/store_data.json";
+import { useState } from "react";
+
+
 export default function Home() {
-
-
+  const [map,setmMap] = useState(null)
+  const storeDatas = stores['DATA']
   return (
     <>
       <Map/>
+      <Markers storeDatas={storeDatas} map={map}/>
     </>
   );
 }
