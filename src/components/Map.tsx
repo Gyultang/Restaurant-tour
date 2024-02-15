03d2d60e1ddf7446921d5c14fb3871bb789a06f1
Gyuri Kim <rnfl0318@naver.com>
2024-02-15 21:34:20 +0900

    map :마커 생성

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,21 +1,39 @@
 /* global kakao */
 import Script from "next/script";
+import * as stores from "@/data/store_data.json";
 
 declare global{
   interface window{
-    kakao: any;
+    kakao : any;
   }
 }
+const DEFAULT_LAT = 37.497625203;
+const DEFAULT_LNG = 127.03088379;
+
 export default function Map() {
   const loadKakaoMAp=()=>{
     // kakao map 로드
     window.kakao.maps.load(()=> {
       const mapContainer = document.getElementById("map")
       const mapOption ={
-        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+        center: new window.kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG),
         level:3,
       }
-      new window.kakao.maps.Map(mapContainer, mapOption)
+      const map = new window.kakao.maps.Map(mapContainer, mapOption)
+
+      // 마커띄우기
+      stores?.['DATA']?.map((store)=>{
+      // 마커가 표시될 위치입니다 
+      var markerPosition  = new window.kakao.maps.LatLng(store?.y_dnts, store?.x_cnts); 
+
+      // 마커를 생성합니다
+      var marker = new window.kakao.maps.Marker({
+          position: markerPosition
+      });
+
+      // 마커가 지도 위에 표시되도록 설정합니다
+      marker.setMap(map);
+      })
   });
   }
 
