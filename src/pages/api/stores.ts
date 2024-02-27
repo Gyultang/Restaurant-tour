617dd03c2e8ca3a0fa9ff83d9972a5c2eb7a43e4
Gyuri Kim <rnfl0318@naver.com>
2024-02-27 22:52:46 +0900

    로그인페이지 수정완 :카카오로그인 구현 필요

--- src/pages/api/stores.ts
+++ src/pages/api/stores.ts
@@ -1,41 +1,34 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import {StoreApiResponse,StoreType} from "@/interface"
-import {PrismaClient} from '@prisma/client'
-
+import { StoreApiResponse, StoreType } from "@/interface";
+import { PrismaClient } from "@prisma/client";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<StoreApiResponse | StoreType[]>,
+  res: NextApiResponse<StoreApiResponse | StoreType[]>
 ) {
-  const {page=''}:{page?:string} = req.query
-  const prisma = new PrismaClient()
+  const { page = "" }: { page?: string } = req.query;
+  const prisma = new PrismaClient();
 
-  if(page){
-    const count = await prisma.store.count()
-    const skipPage = parseInt(page) -1
+  if (page) {
+    const count = await prisma.store.count();
+    const skipPage = parseInt(page) - 1;
     const stores = await prisma.store.findMany({
-      orderBy: {
-        id: 'asc',
-      },
-      take:10,
-      skip: skipPage*10
+      orderBy: { id: "asc" },
+      take: 10,
+      skip: skipPage * 10,
     });
-    // const stores = (await import('../../data/store_data.json'))['DATA'] as StoreType[];
+
     res.status(200).json({
       page: parseInt(page),
-      data:stores,
-      totalCount:count,
-      totalPage:Math.ceil(count/10)
-    })
-  }else{
-    const stores = await prisma.store.findMany({
-      orderBy: {
-        id: 'asc',
-      },
+      data: stores,
+      totalCount: count,
+      totalPage: Math.ceil(count / 10),
     });
-    return stores
-  }
+  } else {
+    const stores = await prisma.store.findMany({
+      orderBy: { id: "asc" },
+    });
 
-  
+    return res.status(200).json(stores);
+  }
 }
--- src/pages/users/login.tsx
+++ src/pages/users/login.tsx
@@ -1,4 +1,21 @@
+import { useEffect } from "react";
+
+import { FaGoogle } from "react-icons/fa";
+import { SiNaver } from "react-icons/si";
+import { RiKakaoTalkFill } from "react-icons/ri";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router"; 
+
+
 export default function LoginPage(){
+    const {status,data:session}=useSession()
+    const router = useRouter()
+    console.log(session)
+    useEffect(()=>{
+        if(status==='authenticated'){
+            router.replace('/')
+        }
+    },[router,status])
     return(
         <div className="flex flex-col justify-center px-6 lg:px-8 h-[60vh]">
             <div className="mx-auto w-full max-w-sm">
@@ -6,6 +23,23 @@ export default function LoginPage(){
                     Nextmap
                 </div>
                 <div className="text-center mt-6 text-2xl font-bold text-gray-600">SNS 계정으로 로그인해주세요</div>
+                <p className="text-center mt-2 text-sm text-gray-600">계정이 없다면 자동으로 회원가입이 진행됩니다.</p>
+            </div>
+            <div className="mt-10 mx-auto w-full max-w-sm">
+                <div className="flex flex-col gap-3">
+                    <button type="button" onClick={()=>signIn('google')} className="text-white flex bg-[#4285f4] hover:bg-[#4285f4]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center gap-3">
+                        <FaGoogle className="h-6 w-6"/>
+                        Sign in with Google
+                    </button>
+                    <button type="button" onClick={()=>signIn('naver')}  className="text-white flex bg-[#2DB400] hover:bg-[#2DB400]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center gap-4">
+                        <SiNaver className="h-5 w-5"/>
+                        Sign in with Naver
+                    </button>
+                    <button type="button" className="text-black flex bg-[#FEE101] hover:bg-[#FEE101]/80 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center gap-4">
+                        <RiKakaoTalkFill className="h-7 w-7"/>
+                        Sign in with Kakao
+                    </button>
+                </div>
             </div>
         </div>
     )
