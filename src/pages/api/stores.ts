a25505533dd2a3636418b86c4546f61262226b13
Gyuri Kim <rnfl0318@naver.com>
2024-02-28 10:32:32 +0900

    상세페이지 구현 :정보 확인 가능 :지도 구현중

--- src/pages/api/stores.ts
+++ src/pages/api/stores.ts
@@ -4,9 +4,9 @@ import { PrismaClient } from "@prisma/client";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<StoreApiResponse | StoreType[]>
+  res: NextApiResponse<StoreApiResponse | StoreType[] | StoreType>
 ) {
-  const { page = "" }: { page?: string } = req.query;
+  const { page = ""}: { page?: string } = req.query;
   const prisma = new PrismaClient();
 
   if (page) {
@@ -25,10 +25,17 @@ export default async function handler(
       totalPage: Math.ceil(count / 10),
     });
   } else {
+    const {id}:{id?:string}=req.query
+
+
     const stores = await prisma.store.findMany({
       orderBy: { id: "asc" },
+      where:{
+        // id값이 있다면 가져오고 없다면 where문을 무시하도록 빈괄호를 넣어줌 {}
+        id: id? parseInt(id):{}
+      }
     });
 
-    return res.status(200).json(stores);
+    return res.status(200).json(id?stores[0]:stores);
   }
 }
